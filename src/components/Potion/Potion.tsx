ea85bfefb4745256e4c64bee90ae583601c094bc
adrivelasco <adrivelasco.dev@gmail.com>
2021-10-19 19:07:50 -0300

    feat: create Potion component

---
+++ src/components/Potion/Potion.tsx
@@ -0,0 +1,30 @@
+import { concat } from 'ramda';
+import { Flex, FlexProps } from '@chakra-ui/react';
+import { CountInput } from '../CountInput';
+import { PotionIcon } from '../Icon';
+
+export interface PotionProps extends FlexProps {
+  color: string;
+}
+
+export const Potion = ({ color, ...props }: PotionProps) => (
+  <Flex position="relative" py={3} alignItems="center" {...props}>
+    <Flex
+      alignItems="center"
+      bg={getBgColor(color)}
+      border="4px solid"
+      borderColor={getBorderColor(color)}
+      borderRadius="full"
+      boxSize={75}
+      justifyContent="center"
+      mr={6}
+    >
+      <PotionIcon boxSize="10" color={getIconColor(color)} />
+    </Flex>
+    <CountInput />
+  </Flex>
+);
+
+const getIconColor = (color: string) => concat(color, '.100');
+const getBgColor = (color: string) => concat(color, '.500');
+const getBorderColor = (color: string) => concat(color, '.700');
